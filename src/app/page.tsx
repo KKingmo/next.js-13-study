aa3c545f05b81366359b7d85c0a26e0518fb407a
kkingmo <55ooccmm@gmail.com>
2023-08-03 18:40:19 +0900

    MUI 연동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,66 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import Drawer from "@mui/material/Drawer";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import MediaCard from "@/components/MediaCard";
+
 export default function Home() {
   return (
-    <>
-      <h2>Welcome</h2>
-      Hello, WEB
-      <p>
-        <img src="/next.svg" width="100" />
-      </p>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+      }}
+    >
+      <Box>
+        <Alert severity="info" sx={{ mt: 2, mb: 5 }}>
+          <AlertTitle>반갑습니다 👋</AlertTitle>
+          Next.js App Router와 Material UI v5붙였습니다.
+        </Alert>
+
+        <Grid container rowSpacing={3} columnSpacing={3}>
+          <Grid xs={6}>
+            <MediaCard heading="가나다라마바사" text="1q2w3e4r!@#" />
+          </Grid>
+          <Grid xs={6}>
+            <MediaCard heading="abcdefg" text="1q2w3e4r!@#" />
+          </Grid>
+          <Grid xs={6}>
+            <MediaCard heading="1234567" text="1q2w3e4r!@#" />
+          </Grid>
+          <Grid xs={6}>
+            <MediaCard heading="한놈이두식이석삼이너구리" text="1q2w3e4r!@#" />
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Drawer
+        sx={{
+          width: 320,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: 320,
+            boxSizing: "border-box",
+            top: ["48px", "56px", "64px"],
+            height: "auto",
+            bottom: 0,
+          },
+        }}
+        variant="permanent"
+        anchor="right"
+      >
+        <List sx={{ px: 2 }}>
+          <ListItem disablePadding>
+            <Typography variant="overline" sx={{ fontWeight: 500 }}>
+              Next 13 버전 파먹기
+            </Typography>
+          </ListItem>
+        </List>
+      </Drawer>
+    </Box>
   );
 }
--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -4,13 +4,29 @@ type ReadProps = {
   };
 };
 
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
 export default function Read(props: ReadProps) {
   const { params } = props;
 
   return (
-    <>
-      <h2>Read</h2>
-      parameter: {params.id}
-    </>
+    <Container>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="body1" gutterBottom>
+          <h2>Read</h2>
+          parameter: {params.id}
+        </Typography>
+      </Box>
+    </Container>
   );
 }
